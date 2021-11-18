528e6a37dc44c70974cd09cff5748534041d6544
zbl <987693976@qq.com>
2021-11-18 18:26:41 +0800

    集成redis

---
+++ src/test/java/com/zbl/springboot/redis/RedisTemplateTest.java
@@ -0,0 +1,33 @@
+package com.zbl.springboot.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @author zbl
+ * @version 1.0
+ * @since 2021/11/18 10:16
+ */
+@SpringBootTest
+public class RedisTemplateTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    @Test
+    public void testRedisConnect() {
+        /*redisTemplate.opsForValue().set("zzz", "zzz的缓存value");*/
+        /*redisTemplate.opsForValue().set("bbb", "bbb的缓存value");*/
+        /*redisTemplate.opsForValue().set("ccc", "ccc的缓存value");*/
+
+        Object zzz = redisTemplate.opsForValue().get("zzz");
+        System.out.println(zzz);
+        Object bbb = redisTemplate.opsForValue().get("bbb");
+        System.out.println(bbb);
+        Object ccc = redisTemplate.opsForValue().get("ccc");
+        System.out.println(ccc);
+    }
+}
