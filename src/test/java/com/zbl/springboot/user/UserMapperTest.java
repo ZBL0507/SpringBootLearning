6d35032c4e173daee9f86eb593bb8bfdbc746ae8
zbl_macOS <1714217989@qq.com>
2022-01-16 17:51:09 +0800

    修复漏洞

--- src/test/java/com/zbl/springboot/user/UserMapperTest.java
+++ src/test/java/com/zbl/springboot/user/UserMapperTest.java
@@ -1,7 +1,9 @@
 package com.zbl.springboot.user;
 
+import com.alibaba.fastjson.JSON;
 import com.zbl.springboot.dao.UserMapper;
 import com.zbl.springboot.po.User;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +15,7 @@ import java.util.Date;
  * @version 1.0
  * @since 2021/11/24 18:02
  */
+@Slf4j
 @SpringBootTest
 @SuppressWarnings("unused")
 public class UserMapperTest {
@@ -33,5 +36,12 @@ public class UserMapperTest {
         user.setAge(25);
         user.setGmtModify(new Date());
         userMapper.updateById(user);
+    }
+
+    @Test
+    public void testSelectById() {
+        User user = userMapper.selectById(12);
+        log.info("user:{}", user.toString());
+        log.info("jsonFormat:{}", JSON.toJSONString(user));
     }
 }
